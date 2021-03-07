8a7b17c9d5abc07ef4a7015dfa71a7f19e7b899b
Peng Chen <pchen0816@outlook.com>
2021-03-06 23:19:36 -0800

    homepage 50% done

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,64 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import ReactTypingEffect from 'react-typing-effect';
 
 export default function Home() {
+
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Peng Chen's work</title>
+        <title>Hi, I am Peng</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={styles.background}>
+        <div className={styles.container}>
+          <h1 className={styles.text_h1}>
+            <section>
+              <Link href='/about'>
+                <a>
+                  <div className={`${styles.text_color_white} ${styles.on_hover} ${styles.first_line}`}><span>Hi,</span></div>
+                </a>
+              </Link>
+            </section>
+            <section>
+              <Link href='/work'>
+                <a>
+                  <div className={`${styles.text_color_red} ${styles.on_hover} ${styles.second_line}`}><span>I am</span></div>
+                </a>
+              </Link>
+            </section>
+            <section>
+              <Link href='/contact'>
+                <a>
+                  <div className={`${styles.text_color_red} ${styles.on_hover} ${styles.third_line}`}><span>Peng,</span></div>
+                </a>
+              </Link>
+            </section>
+          </h1>
+          <h2 className={styles.text_h2}>
+            <section>
+              <Link href="/blog">
+                <a>
+                  <div className={`${styles.text_color_white} ${styles.on_hover} ${styles.fourth_line}`}>
+                    <span>
+                      <ReactTypingEffect 
+                        text={["a web developer.", "an Army Veteran.", 'a husband and father.']}
+                        typingDelay = {0}
+                        eraseDelay = {1500}
+                      />
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            </section>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Sorry, this site is still under development.
-        </h1>
-
-        <div className={styles.grid}>
-          <a href="https://github.com/PengChen11/pengchen.work" className={styles.card}>
-            <h3>Front end GitHub repo &rarr;</h3>
-            <p>The front end is still under development with NextJS, a great React framework designed for SSR.</p>
-          </a>
-
-          <a href="https://github.com/PengChen11/Microservices-API_Gateway" className={styles.card}>
-            <h3>Backend GitHub Repo &rarr;</h3>
-            <p>The Back end is supported by a series of microservices built with Node.js </p>
-          </a>
-
+          </h2>
+          <div className={styles.tap_indication}>Click anything</div>
         </div>
+        
       </main>
-
-      
-    </div>
+    </>
   )
 }
